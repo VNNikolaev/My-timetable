0e7fc143e218a2f02dd29985b15c065cda375d09
Vasiiy <nikolaev.vasiliy.96@mail.ru>
2020-02-16 01:17:44 +0500

    Initial Commit

---
+++ My timetable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My timetable
+//
+//  Created by Васлий Николаев on 16.02.2020.
+//  Copyright © 2020 Васлий Николаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
